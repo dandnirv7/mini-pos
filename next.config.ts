acb54994c711c0fbb645c88f8b6a8f365c3fd8b3
dandnirv7 <dandnirv7@gmail.com>
2025-02-17 22:05:34 +0700

    chore: remove unused headers configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,47 +5,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_API_URL_DEV: process.env.BASE_URL_DEV,
     NEXT_PUBLIC_API_URL_PROD: process.env.BASE_URL_PROD,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'; object-src 'none';",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
